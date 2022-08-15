b658aa15e84fd877bbfab3a8bdecedb579eecdf7
Selami Demir <selamidemir@gmail.com>
2022-08-15 18:30:05 +0300

    First commit. Created app with react-create-app and added redux to the app. Edited the title and description. Deleted some unnecessary html code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>Typeed</h2>
+    <div>
+      speed testing
+    </div>
+    </div>
+  );
+}
+
+export default App;
